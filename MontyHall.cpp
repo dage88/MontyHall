a0a2eed4ebcd98d962ad0ac5f84a57083e9c4062
Da Ge <dage@gmx.net>
2021-11-27 00:56:03 +0100

    Establish initial version with rand() and always selecting gate 1

---
+++ MontyHall.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <string>
+#include <random>
+using namespace std;
+
+/* Monty Hall simulation, with always picking the first gate, after which the moderator is forced to discard either gate 2 or 3 */
+int main()
+{
+    typedef enum Gate {
+        uninit,
+        mainprice,
+        looser
+    }Gate_t; 
+
+    Gate* offeredGate_p = NULL;
+    int stays=0, changes=0;
+    float RNG = 0;
+    Gate_t GateOne=uninit, GateTwo=uninit, GateThree=uninit;
+
+    for (int i=0; i<1000; i++)
+    {
+        GateOne=uninit;
+        GateTwo=uninit;
+        GateThree=uninit;
+        
+        RNG = float(rand())/RAND_MAX;
+
+        if ((RNG>=0) && (RNG<=1))
+        {
+            if(RNG<0.33)
+            {
+                GateOne=mainprice;
+                GateTwo=looser;
+                GateThree=looser;
+            }
+            else if (RNG>0.66)
+            {
+                GateOne=looser;
+                GateTwo=looser;
+                GateThree=mainprice;
+            }
+            else
+            {
+                GateOne=looser;
+                GateTwo=mainprice;
+                GateThree=looser;
+            }
+        }
+        else
+        {
+            cout << "RNG out of range!" << endl;
+        }
+    
+        if ((GateOne!=uninit)&&(GateTwo!=uninit)&&(GateThree!=uninit))
+        {
+            /* Always pick gate 1 first, moderator discards the looser 2/3 */
+            if (GateTwo == mainprice)
+            {
+                offeredGate_p = &GateTwo;
+            }
+            else if (GateThree == mainprice)
+            {
+                offeredGate_p = &GateThree;
+            }
+            else
+            {
+                /*GateOne is mainprice. Randomly offer gate 2 or 3 */
+                if (float(rand()/RAND_MAX)<0,5) 
+                {
+                    offeredGate_p = &GateTwo;
+                    }
+                else
+                {
+                    offeredGate_p = &GateThree;
+                }
+            }
+
+            /* Scenario 1: Hero stays with GateOne */
+            if (GateOne == mainprice)
+            {
+                stays++;
+            }
+
+            /* Scenario 2: Hero changes to offered gate */
+           if (*offeredGate_p == mainprice)
+           {
+               changes++;
+           }    
+        }
+        else
+        {
+            cout << "Gate uninitialized" << endl;
+        }
+    }
+
+cout << "Times, when Hero was right be staying with his initial choice gateOne (stays): " << stays << endl;
+cout << "Times, when Hero was right be changing his initial choice and takes the offered gate (changes): " << changes << endl;   
+}
